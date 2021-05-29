5cea956d57244d30684b8ec818d1b78246f94a8e
Sherief Elsowiny <elsowiny@yahoo.com>
2021-05-28 21:05:11 -0400

    added functionality in app that holds the user object from firestore as a state object which we can access later

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,29 @@ const config = {
     appId: "1:44910399786:web:35093c7659e2505aa04e53"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+  
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if(!snapShot.exists){
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user ', error.message);
+      }
+    }
+    return userRef;
+};
+  
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
